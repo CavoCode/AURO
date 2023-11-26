85c3059d8f758bfe5b31ca2a588ab8588bbf8502
CavoCode <michaelcavaciuti@gmail.com>
2023-11-26 18:35:16 +0000

    Num_Robots on Launch && FSM Begun
    Created automated method for spawning robots from solution launch file (with controllers mapped to correct cmd_vel)
    
    Also begun creating FSM for overall states.

--- Submission/src/solution/launch/solution_launch.py
+++ Submission/src/solution/launch/solution_launch.py
@@ -1,11 +1,59 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction, LogInfo, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
+def group_action(context : LaunchContext):
+    num_robots = int(context.launch_configurations['num_robots'])
+
+    robots_list = {}
+
+    match num_robots:
+        case 1:
+            robots_list["robot1"] = {}
+            robots_list["robot1"]["y"] =  0.0
+        case 2:
+            robots_list["robot1"] = {}
+            robots_list["robot1"]["y"] =  1.0
+
+            robots_list["robot2"] = {}
+            robots_list["robot2"]["y"] = -1.0
+        case _:
+            robots_list["robot1"] = {}
+            robots_list["robot1"]["y"] =  2.0
+
+            robots_list["robot2"] = {}
+            robots_list["robot2"]["y"] =  0.0
+
+            robots_list["robot3"] = {}
+            robots_list["robot3"]["y"] = -2.0
+
+    bringup_cmd_group = []
+
+    for robot_name in robots_list:
+        group = GroupAction([
+            robot_controller_cmd = Node(
+            package='solution',
+            executable='robot_controller',
+            output='screen',
+            namespace=robot_name)
+        ])
+        
+        bringup_cmd_group.append(group)
+
+    return bringup_cmd_group
+
+
 def generate_launch_description():
+
+    num_robots = LaunchConfiguration('num_robots')
+
+    declare_sol_num_robots_cmd = DeclareLaunchArgument(
+        'num_robots',
+        default_value='1',
+        description='Number of robots to spawn')
 
     assessment_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -15,7 +63,7 @@ def generate_launch_description():
                 'assessment_launch.py'
                 ])
         ),
-        launch_arguments={'num_robots': '1',
+        launch_arguments={'num_robots': num_robots,
                           'visualise_sensors': 'false',
                           'use_rviz': 'true',
                           'obstacles': 'true',
@@ -24,15 +72,12 @@ def generate_launch_description():
                           }.items()
     )
 
-    robot_controller_cmd = Node(
-        package='solution',
-        executable='robot_controller',
-        output='screen',
-        namespace='robot1')
+    bringup_cmd_group = OpaqueFunction(function=group_action)
 
     ld = LaunchDescription()
 
+    ld.add_action(declare_sol_num_robots_cmd)
     ld.add_action(assessment_cmd)
-    ld.add_action(robot_controller_cmd)
+    ld.add_action(bringup_sol_cmd_group)
 
     return ld
--- Submission/src/solution/solution/robot_controller.py
+++ Submission/src/solution/solution/robot_controller.py
@@ -38,9 +38,9 @@ class Machine_State(Enum):
     IDLE = 0
     EXPLORE = 1
     ASSESS_ITEM_ENVIRONMENT = 2
-    APPROACH_ITEM = 3
-    COLLECT_ITEM = 4
-    GO_HOME = 5
+    COLLECT_ITEM = 3
+    GO_HOME = 4
+    ERROR_DETECTED = 5
 
 #FSM states for movement
 class Movement_State(Enum):
@@ -145,21 +145,34 @@ class RobotController(Node):
 
         # Send message to rviz_text_marker node
         marker_input = StringWithPose()
-        marker_input.text = str(self.state) # Visualise robot state as an RViz marker
+        marker_input.text = str(self.movement_state) # Visualise robot movement state as an RViz marker
         marker_input.pose = self.pose # Set the pose of the RViz marker to track the robot's pose
         self.marker_publisher.publish(marker_input)
 
-        # self.get_logger().info(f"{self.state}")
+        self.get_logger().info(f"Robot State: {self.machine_state}")
+        self.get_logger().info(f"Movement State: {self.movement_state}")
 
         match self.machine_state:
 
             case Machine_State.IDLE:
-                #Check if robot is holding item - if yes head home, if no check if robot is in home
-                #if robot is home, and facing wall, turn till more items are visible from home
-                #if robot is not home and not holding item transition to EXPLORE state
-                #if robot is home transition to EXPLORE state also
-                #if robot is not at home and carrying item with higher value item in sight, 
-                1 == 1
+                '''
+                self.movement_state = Movement_State.Stationary
+
+                if error_detected():
+                    self.machine_state = Machine_State.ERROR_DETECTED
+
+                if is_robot_available() && is_robot_home(): 
+                    #DO CHECKS FOR IF MACHINE IS FACING WALL - TURN TO FACE OPEN MAP RATHER THAN HOME
+
+                    #THEN SET MACHINE STATE TO EXPLORE
+                    self.machine_state = Machine_State.EXPLORE
+                else if is_robot_carrying():
+                    #DO CHECK IF HIGHER VALUE TARGET IS IN SITE - If yes transition to ASSESS_ITEM_ENVIRONMENT
+                    #else
+                    self.machine_state = Machine_State.GO_HOME
+                else:
+                    self.machine_state = Machine_State.EXPLORE
+                '''
 
             case Machine_State.EXPLORE:
                 1 == 1
@@ -173,17 +186,15 @@ class RobotController(Node):
                 #Update global location of last seen highest value target
                 #So other robots can approach area
 
-            case Machine_State.APPROACH_ITEM:
-                1 == 1
-
             case Machine_State.COLLECT_ITEM:
                 1 == 1
                 #After collection always move to assess_item_environment
                 #to see if higher value target is within sight and reasonable distance
 
-                
-
             case Machine_State.GO_HOME:
+                1 == 1
+
+            case Machine_State.ERROR_DETECTED:
                 1 == 1
 
             case _:
@@ -192,10 +203,48 @@ class RobotController(Node):
         match self.movement_state:
 
             case Movement_State.FORWARD:
-                1 == 1
+                if self.scan_triggered[SCAN_FRONT]:
+                    self.previous_yaw = self.yaw
+                    self.state = State.TURNING
+                    self.turn_angle = random.uniform(150, 170)
+                    self.turn_direction = random.choice([TURN_LEFT, TURN_RIGHT])
+                    self.get_logger().info("Detected obstacle in front, turning " + ("left" if self.turn_direction == TURN_LEFT else "right") + f" by {self.turn_angle:.2f} degrees")
+                    return
+                
+                if self.scan_triggered[SCAN_LEFT] or self.scan_triggered[SCAN_RIGHT]:
+                    self.previous_yaw = self.yaw
+                    self.state = State.TURNING
+                    self.turn_angle = random.uniform(45, 90)
+
+                    if self.scan_triggered[SCAN_LEFT] and self.scan_triggered[SCAN_RIGHT]:
+                        self.turn_direction = random.choice([TURN_LEFT, TURN_RIGHT])
+                        self.get_logger().info("Detected obstacle to both the left and right, turning " + ("left" if self.turn_direction == TURN_LEFT else "right") + f" by {self.turn_angle:.2f} degrees")
+                    elif self.scan_triggered[SCAN_LEFT]:
+                        self.turn_direction = TURN_RIGHT
+                        self.get_logger().info(f"Detected obstacle to the left, turning right by {self.turn_angle} degrees")
+                    else: # self.scan_triggered[SCAN_RIGHT]
+                        self.turn_direction = TURN_LEFT
+                        self.get_logger().info(f"Detected obstacle to the right, turning left by {self.turn_angle} degrees")
+                    return
+
+                msg = Twist()
+                msg.linear.x = LINEAR_VELOCITY
+                self.cmd_vel_publisher.publish(msg)
+
 
             case Movement_State.TURNING:
-                1 == 1
+
+                msg = Twist()
+                msg.angular.z = self.turn_direction * ANGULAR_VELOCITY
+                self.cmd_vel_publisher.publish(msg)
+
+                yaw_difference = angles.normalize_angle(self.yaw - self.previous_yaw)
+
+                if math.fabs(yaw_difference) >= math.radians(self.turn_angle):
+                    self.goal_distance = random.uniform(1.0, 2.0)
+                    self.state = State.FORWARD
+                    self.get_logger().info(f"Finished turning, driving forward by {self.goal_distance:.2f} metres")
+
 
             case Movement_State.STATIONARY:
                 1 == 1
@@ -205,6 +254,9 @@ class RobotController(Node):
 
 
     def destroy_node(self):
+        msg = Twist()
+        self.cmd_vel_publisher.publish(msg)
+        self.get_logger().info(f"Stopping: {msg}")
         super().destroy_node()
 
 
